2cd3a0dbe0d477c2ca46d88b82d172b4e6edfd7d
Diastro <erwald@erwald-System-Product-Name.(none)>
2012-12-30 00:44:16 -0500

    Adding 01fork.spp

---
+++ src/Process/Basics/01fork.cpp
@@ -0,0 +1,51 @@
+/**
+ * File 			: 01fork.cpp
+ * Date of creation : December 29 2012
+ * Author 			: David Albertson
+ * Twitter 			: @DavidAlbertson
+ * Website			: www.dalbertson.com
+ *
+ * Description : 
+ * Creation of a child process and understanding how the chilPID value changes depending on which process we're in.
+ *
+ * References :
+ * - www.die.net
+ * - http://publib.boulder.ibm.com/infocenter/zos/v1r11/index.jsp?topic=/com.ibm.zos.r11.bpxbd00/rtgtpp.htm
+ */
+
+#include <unistd.h>		// for : fork()
+#include <sys/types.h>	// for : pid_t
+#include <stdio.h>		// for : perror, printf
+#include <errno.h>		// for : errno
+
+int main(int argc, char *arg[])
+{
+    pid_t childPID;		// Used to store the processID of the child after the fork() call
+    childPID = fork();	// Creation of a new process (a child process)
+
+    // At this point, when the fork() call returns, a new (identical) process will be created.
+    // If we are in the newly created process (the childProcess), the childPID variable will be 0.
+    // Otherwise, if we are in the parent process the childPID variable will hold the value of the child PID.
+    if(childPID >= 0) // The fork() call was successful
+    {
+        if(childPID == 0) // Code executed in the child process
+        {
+           printf("Child Process ID : %i .\n", getpid());
+           printf("Child Parent Process ID : %i .\n", getppid());
+        }
+        else // Code executed in the parent process
+        {
+           printf("Parent Process ID :%i .\n", getpid());
+        }
+    }
+    else if(childPID <0) // The fork call failed
+    {
+        printf("\n Fork failed exiting...\n");
+        return 1;
+    }
+
+    sleep(1); // Simply to make the output clear, printing this text at the end.
+    printf("Printed by both process : %i .\n", getpid()); // Code executed by both process
+
+    return 0;
+}
